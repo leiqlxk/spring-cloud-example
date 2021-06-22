d5a6725b444c88e3efa177906c1bf97157421870
leiqlxk <472990991@qq.com>
2021-06-22 13:07:12 +0800

    eureka security

---
+++ ch3-eureka-server/src/main/java/org/lql/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.lql.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Title: SecurityConfig <br>
+ * ProjectName: spring-cloud-example <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/6/22 10:55 <br>
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
