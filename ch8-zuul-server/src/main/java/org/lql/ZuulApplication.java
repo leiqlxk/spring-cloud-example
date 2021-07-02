916f264f70bdda66148a23800d0c016bfaff738a
leiqlxk <472990991@qq.com>
2021-07-02 17:12:19 +0800

    zuul

--- ch8-zuul-server/src/main/java/org/lql/ZuulApplication.java
+++ ch8-zuul-server/src/main/java/org/lql/ZuulApplication.java
@@ -2,11 +2,8 @@ package org.lql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Title: ZuulApplication <br>
@@ -20,14 +17,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableOAuth2Sso
-public class ZuulApplication extends WebSecurityConfigurerAdapter {
+//@EnableOAuth2Sso
+//public class ZuulApplication extends WebSecurityConfigurerAdapter {
+public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
 
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login")
@@ -37,5 +35,5 @@ public class ZuulApplication extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
-    }
+    }*/
 }
