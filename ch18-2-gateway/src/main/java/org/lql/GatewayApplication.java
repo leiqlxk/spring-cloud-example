78d596b18867cfef9069d668fbe9ae794f992f21
leiqlxk <472990991@qq.com>
2021-07-19 17:16:43 +0800

    gateway

---
+++ ch18-2-gateway/src/main/java/org/lql/GatewayApplication.java
@@ -0,0 +1,21 @@
+package org.lql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Title: GatewayApplication <br>
+ * ProjectName: spring-cloud-example <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/7/19 16:18 <br>
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
